2fc2f553317c6000851d4fe40b0dde49e837bb90
Tomislav Maricevic <tmarice@gmail.com>
2021-12-15 21:45:37 +0100

    Initial timed test runner

--- django_timed_tests/runner.py
+++ django_timed_tests/runner.py
@@ -1,4 +1,25 @@
+from time import time
+from unittest import TextTestResult, TextTestRunner
 from django.test.runner import DiscoverRunner
 
+
+class TimedTextTestResult(TextTestResult):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._starts = {}
+        self.durations = {}
+
+    def startTest(self, test):
+        self._starts[test] = time()
+
+    def addSuccess(self, test):
+        self.durations[test] = time() - self._starts[test]
+
+
+class TimedTextTestRunner(TextTestRunner):
+    resultclass = TimedTextTestResult
+
+
 class TimedTestRunner(DiscoverRunner):
-    pass
+    test_runner = TimedTextTestRunner
