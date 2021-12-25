fc7103101799b29f3609c81c9e80ef336489a722
Tomislav Maricevic <tmarice@gmail.com>
2021-12-25 19:24:07 +0100

    Handle parallel runs

--- django_timed_tests/runner.py
+++ django_timed_tests/runner.py
@@ -1,6 +1,23 @@
 from time import time
 from unittest import TextTestResult, TextTestRunner
-from django.test.runner import DiscoverRunner
+from django.test.runner import DiscoverRunner, RemoteTestResult, RemoteTestRunner, ParallelTestSuite
+
+
+class TimedRemoteTestResult(RemoteTestResult):
+    def startTest(self, test):
+        self.testsRun += 1
+        self.events.append(('startTest', self.test_index, time()))
+
+    def addSuccess(self, test):
+        self.events.append(('addSuccess', self.test_index, time()))
+
+
+class TimedRemoteTestRunner(RemoteTestRunner):
+    resultclass = TimedRemoteTestResult
+
+
+class TimedParallelTestSuite(ParallelTestSuite):
+    runner_class = TimedRemoteTestRunner
 
 
 class TimedTextTestResult(TextTestResult):
@@ -10,11 +27,21 @@ class TimedTextTestResult(TextTestResult):
         self._starts = {}
         self.durations = {}
 
-    def startTest(self, test):
-        self._starts[test] = time()
+    def startTest(self, test, start_time=None):
+        if start_time is None:
+            start_time = time()
 
-    def addSuccess(self, test):
-        self.durations[test] = time() - self._starts[test]
+        self._starts[test] = start_time
+
+        return super().startTest(test)
+
+    def addSuccess(self, test, end_time=None):
+        if end_time is None:
+            end_time = time()
+
+        self.durations[test] = end_time - self._starts[test]
+
+        return super().addSuccess(test)
 
 
 class TimedTextTestRunner(TextTestRunner):
@@ -23,3 +50,4 @@ class TimedTextTestRunner(TextTestRunner):
 
 class TimedTestRunner(DiscoverRunner):
     test_runner = TimedTextTestRunner
+    parallel_test_suite = TimedParallelTestSuite
