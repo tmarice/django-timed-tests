ff6a0dcbfd85d8619653d0cdbc8cf1372c503921
Tomislav Maricevic <tmarice@gmail.com>
2021-12-25 19:25:09 +0100

    Fix tests

--- django_timed_tests/tests/examples.py
+++ django_timed_tests/tests/examples.py
@@ -1,7 +1,33 @@
 from time import sleep
 from django.test import TestCase
 
-class DummyTestCase(TestCase):
+class DummyTestCase1(TestCase):
+    def test_dummy_1(self):
+        self.assertTrue(True)
+        sleep(1)
+
+    def test_dummy_2(self):
+        self.assertTrue(True)
+        sleep(2)
+
+    def test_dummy_3(self):
+        self.assertTrue(True)
+        sleep(3)
+
+class DummyTestCase2(TestCase):
+    def test_dummy_1(self):
+        self.assertTrue(True)
+        sleep(1)
+
+    def test_dummy_2(self):
+        self.assertTrue(True)
+        sleep(2)
+
+    def test_dummy_3(self):
+        self.assertTrue(True)
+        sleep(3)
+
+class DummyTestCase3(TestCase):
     def test_dummy_1(self):
         self.assertTrue(True)
         sleep(1)
--- django_timed_tests/tests/tests.py
+++ django_timed_tests/tests/tests.py
@@ -1,15 +1,53 @@
 from django.test import TestCase
+import multiprocessing
+import os
+import sys
 from io import StringIO
 from django_timed_tests.runner import TimedTestRunner
 from django.test.runner import DiscoverRunner
 
+
+
 class DummyTestCase(TestCase):
     def test_dummy(self):
-        # test_runner = TimedTestRunner()
-        django_test_runner = DiscoverRunner()
+        django_test_runner = TimedTestRunner()
         suite = django_test_runner.build_suite(["examples"])
         runner_kwargs = django_test_runner.get_test_runner_kwargs()
         output_stream = StringIO()
         runner_kwargs['stream'] = output_stream
         test_runner = django_test_runner.test_runner(**runner_kwargs)
         result = test_runner.run(suite)
+
+        for test, duration in result.durations.items():
+            expected_duration = int(test._testMethodName.split('_')[-1])
+            self.assertAlmostEqual(expected_duration, duration, places=1)
+
+
+class DummyParallelTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        if sys.platform == "darwin" and os.environ.get("OBJC_DISABLE_INITIALIZE_FORK_SAFETY", "") != "YES":
+            print(
+                (
+                    "Set OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES in your"
+                    + " environment to work around use of forking in Django's"
+                    + " test runner."
+                ),
+                file=sys.stderr,
+            )
+            sys.exit(1)
+
+        multiprocessing.set_start_method("fork")
+
+    def test_dummy_parallel(self):
+        django_test_runner = TimedTestRunner(parallel=3)
+        suite = django_test_runner.build_suite(["examples"])
+        runner_kwargs = django_test_runner.get_test_runner_kwargs()
+        output_stream = StringIO()
+        runner_kwargs['stream'] = output_stream
+        test_runner = django_test_runner.test_runner(**runner_kwargs)
+        result = test_runner.run(suite)
+
+        for test, duration in result.durations.items():
+            expected_duration = int(test._testMethodName.split('_')[-1])
+            self.assertAlmostEqual(expected_duration, duration, places=1)
