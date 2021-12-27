b4f579790d8ecd60318c0963f2a4bbebe32c9ac3
Tomislav Maricevic <tmarice@gmail.com>
2021-12-27 12:29:04 +0100

    Refactor tests

--- tests/tests.py
+++ tests/tests.py
@@ -2,30 +2,17 @@ import multiprocessing
 import os
 import sys
 from io import StringIO
+from time import time
 
 from django.test import TestCase
 
 from django_timed_tests.runner import TimedTestRunner
 
 EXAMPLE_TEST_SUITE_PATH = "tests.examples"
+TOTAL_TEST_DURATION = 18
 
 
-class DummyTestCase(TestCase):
-    def test_dummy(self):
-        django_test_runner = TimedTestRunner()
-        suite = django_test_runner.build_suite([EXAMPLE_TEST_SUITE_PATH])
-        runner_kwargs = django_test_runner.get_test_runner_kwargs()
-        output_stream = StringIO()
-        runner_kwargs["stream"] = output_stream
-        test_runner = django_test_runner.test_runner(**runner_kwargs)
-        result = test_runner.run(suite)
-
-        for test, duration in result.durations.items():
-            expected_duration = int(test._testMethodName.split("_")[-1])
-            self.assertAlmostEqual(expected_duration, duration, places=1)
-
-
-class DummyParallelTestCase(TestCase):
+class TimedTestRunnerTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         if sys.platform == "darwin" and os.environ.get("OBJC_DISABLE_INITIALIZE_FORK_SAFETY", "") != "YES":
@@ -41,15 +28,26 @@ class DummyParallelTestCase(TestCase):
 
         multiprocessing.set_start_method("fork")
 
-    def test_dummy_parallel(self):
-        django_test_runner = TimedTestRunner(parallel=3)
+    def _test_run(self, parallel=1):
+        start = time()
+        django_test_runner = TimedTestRunner(parallel=parallel)
         suite = django_test_runner.build_suite([EXAMPLE_TEST_SUITE_PATH])
         runner_kwargs = django_test_runner.get_test_runner_kwargs()
         output_stream = StringIO()
         runner_kwargs["stream"] = output_stream
         test_runner = django_test_runner.test_runner(**runner_kwargs)
+
         result = test_runner.run(suite)
+
+        duration = time() - start
+        self.assertAlmostEqual(duration, TOTAL_TEST_DURATION / parallel, places=0)
 
         for test, duration in result.durations.items():
             expected_duration = int(test._testMethodName.split("_")[-1])
             self.assertAlmostEqual(expected_duration, duration, places=1)
+
+    def test_sequential(self):
+        self._test_run()
+
+    def test_parallel(self):
+        self._test_run(parallel=3)
